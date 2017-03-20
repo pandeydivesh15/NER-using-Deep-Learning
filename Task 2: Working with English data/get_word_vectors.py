ae96fc32a06eef1b138ab385c6c0dd88395da333
Divesh Pandey <divesh.pandey.cse15@iitbhu.ac.in>
2017-03-20 10:05:20 +0530

    Minor change in get_word_vector_script

--- Task 2: Working with English data/get_word_vectors.py
+++ Task 2: Working with English data/get_word_vectors.py
@@ -5,12 +5,15 @@ NLP = spacy.load("en")
 
 def get_word_vectors(sentence):
 	"""
-	Returns word vectors for complete sentence / single word
-	as a python list"""
-
+	Returns word vectors for complete sentence as a python list"""
 	s = NLP(unicode(sentence))
 	vec = [ word.vector for word in s ]
 	return vec
 
+def get_word_vector(word):
+	"""
+	Returns word vectors for a single word as a python list"""
 
+	s = NLP(unicode(word))
+	return s.vector
 	
